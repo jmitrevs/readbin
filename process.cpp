2fa7a180867408fe378f9ba41dcb3821c8e78cd1
Jovan Mitrevski <jmitrevs@fnal.gov>
2022-03-24 18:16:08 -0500

    add a kernel

--- process.cpp
+++ process.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <sstream>
 #include <stdexcept>
+#include <vector>
 #include "daqdataformats/TriggerRecordHeaderData.hpp"
 #include "daqdataformats/FragmentHeader.hpp"
 #include "daqdataformats/ComponentRequest.hpp"
@@ -10,10 +11,10 @@
 #include "readbin.h"
 
 // process a trigger record and subsequent data
-void process(uint8_t readbuf[READ_SIZE], uint8_t writebuf[WRITE_SIZE], int numToRead, off_t& numRead, off_t& numWritten) {
+void process(uint8_t readbuf[READ_SIZE], uint8_t writebuf[WRITE_SIZE], int numToRead, off_t& num_read, off_t& num_written) {
 
-    numRead = 0;
-    numWritten = 0;
+    num_read = 0;
+    num_written = 0;
 
     // The dummy processing now just prints out some info
     std::ostringstream outputtext;
@@ -27,20 +28,20 @@ void process(uint8_t readbuf[READ_SIZE], uint8_t writebuf[WRITE_SIZE], int numTo
         throw std::runtime_error(error_msg.str());
     }
 
-    numRead += sizeof(dunedaq::daqdataformats::TriggerRecordHeaderData);
+    num_read += sizeof(dunedaq::daqdataformats::TriggerRecordHeaderData);
     outputtext << "Trigger record:\t" << *trigRecHeader << std::endl;
 
     // this should be followed by component request
-    const auto compReq = reinterpret_cast<dunedaq::daqdataformats::ComponentRequest *>(&readbuf[numRead]);
+    const auto compReq = reinterpret_cast<dunedaq::daqdataformats::ComponentRequest *>(&readbuf[num_read]);
 
-    numRead += sizeof(dunedaq::daqdataformats::ComponentRequest);
+    num_read += sizeof(dunedaq::daqdataformats::ComponentRequest);
     outputtext << "Requst:\t" << *compReq << std::endl;
 
     for (int comp = 0; comp < trigRecHeader->num_requested_components; ++comp) {
-        if (READ_SIZE - numRead < sizeof(dunedaq::daqdataformats::FragmentHeader)) {
+        if (READ_SIZE - num_read < sizeof(dunedaq::daqdataformats::FragmentHeader)) {
             throw std::runtime_error("The read buffer is too small to contain the fragment header");
         }            
-        const auto fragmentHeader = reinterpret_cast<dunedaq::daqdataformats::FragmentHeader *>(&readbuf[numRead]);
+        const auto fragmentHeader = reinterpret_cast<dunedaq::daqdataformats::FragmentHeader *>(&readbuf[num_read]);
         if (fragmentHeader->fragment_header_marker != dunedaq::daqdataformats::FragmentHeader::s_fragment_header_magic) {
             std::ostringstream error_msg;
             error_msg << "The parsed fragment header did not have the right magic number: " << std::hex << fragmentHeader->fragment_header_marker
@@ -49,35 +50,51 @@ void process(uint8_t readbuf[READ_SIZE], uint8_t writebuf[WRITE_SIZE], int numTo
         }
 
         // make sure that all the fragments were read in
-        if (numRead + fragmentHeader->size > READ_SIZE) {
+        if (num_read + fragmentHeader->size > READ_SIZE) {
             throw std::runtime_error("The read buffer is too small to contain the fragment");
         }
 
-        numRead += sizeof(dunedaq::daqdataformats::FragmentHeader);
+        num_read += sizeof(dunedaq::daqdataformats::FragmentHeader);
         outputtext << "Fragment:\t" << *fragmentHeader << std::endl;
 
         // let's see the number of frames
-        const auto framesSize = fragmentHeader->size - sizeof(dunedaq::daqdataformats::FragmentHeader);
-        const int numFrames = framesSize / sizeof(dunedaq::detdataformats::wib::WIBFrame);
+        const auto frames_size = fragmentHeader->size - sizeof(dunedaq::daqdataformats::FragmentHeader);
+        const int num_frames = frames_size / sizeof(dunedaq::detdataformats::wib::WIBFrame);
 
-        for (int iframe = 0; iframe < numFrames; ++iframe) {
-            const auto frame = reinterpret_cast<dunedaq::detdataformats::wib::WIBFrame *>(&readbuf[numRead]);
 
-            // to control the output size, only print the first 10
-            if (iframe < 10) {
-                outputtext << *frame;
+        constexpr int num_channels = dunedaq::detdataformats::wib::ColdataBlock::s_num_ch_per_block 
+            * dunedaq::detdataformats::wib::WIBFrame::s_num_block_per_frame;
+
+        outputtext << "num_frames = " << num_frames << ", num channels per block = " 
+            << dunedaq::detdataformats::wib::ColdataBlock::s_num_ch_per_block 
+            << ", num_channels = " << num_channels << std::endl;
+
+        std::vector<uint16_t> channels[num_channels];
+
+        for (int iframe = 0; iframe < num_frames; ++iframe) {
+            const auto frame = reinterpret_cast<dunedaq::detdataformats::wib::WIBFrame *>(&readbuf[num_read]);
+
+            if (iframe == 0) {
+                outputtext << "Slot = " << frame->get_wib_header()->slot_no 
+                    << ", fiber = " << frame->get_wib_header()->fiber_no << std::endl;
             }
-            numRead += sizeof(dunedaq::detdataformats::wib::WIBFrame);
+
+            for (int ich = 0; ich < num_channels; ++ich) {
+                channels[ich].push_back(frame->get_channel(ich));
+            }
+            num_read += sizeof(dunedaq::detdataformats::wib::WIBFrame);
         }
-        
+        for (int ich = 0; ich < num_channels; ++ich) {
+            std::cout << "channels[" << ich << "] has size " <<  channels[ich].size() << std::endl;
+        }
     }
 
     auto outputstr = outputtext.str();
 
-    numWritten = outputstr.size();
+    num_written = outputstr.size();
 
-    if (numWritten > WRITE_SIZE) {
-        std::cerr << "numWritten = " << std::hex << numWritten << " is too large" << std::endl;
+    if (num_written > WRITE_SIZE) {
+        std::cerr << "num_written = " << std::hex << num_written << " is too large" << std::endl;
     }
     for (int i = 0; i < outputstr.size(); i++) {
         writebuf[i] = outputstr[i];
---
+++ process_kernel.cpp
@@ -0,0 +1,87 @@
+//#include <unistd.h>
+#include <iostream>
+//#include <cstdint>
+//#include <sstream>
+//#include <stdexcept>
+//#include <vector>
+#include "daqdataformats/TriggerRecordHeaderData.hpp"
+#include "daqdataformats/FragmentHeader.hpp"
+#include "daqdataformats/ComponentRequest.hpp"
+#include "detdataformats/wib/WIBFrame.hpp"
+#include "readbin.h"
+
+// process a trigger record and subsequent data
+
+constexpr int NUM_CHANNELS = dunedaq::detdataformats::wib::ColdataBlock::s_num_ch_per_block 
+        * dunedaq::detdataformats::wib::WIBFrame::s_num_block_per_frame;
+
+
+void process(uint8_t readbuf[READ_SIZE], off_t& num_read, uint16_t channels[NUM_CHANNELS]) {
+
+    #pragma HLS ARRAY_PARTITION variable=ret_val complete
+
+    // initialize
+    num_read = 0;
+    for (int ich = 0; ich < NUM_CHANNELS; ++ich) {
+        #pragma HLS unroll
+        channels[ich] = 0;
+    }
+
+    const auto trigRecHeader = reinterpret_cast<dunedaq::daqdataformats::TriggerRecordHeaderData *>(readbuf);
+
+    if (trigRecHeader->trigger_record_header_marker != dunedaq::daqdataformats::TriggerRecordHeaderData::s_trigger_record_header_magic) {
+        
+        std::cerr << "The parsed trigger record header did not have the right magic number: " << std::hex <<  trigRecHeader->trigger_record_header_marker
+                  << " (expected " << dunedaq::daqdataformats::TriggerRecordHeaderData::s_trigger_record_header_magic << ")\n";
+        return;
+    }
+
+    num_read += sizeof(dunedaq::daqdataformats::TriggerRecordHeaderData);
+
+    // this should be followed by component request
+    const auto compReq = reinterpret_cast<dunedaq::daqdataformats::ComponentRequest *>(&readbuf[num_read]);
+
+    num_read += sizeof(dunedaq::daqdataformats::ComponentRequest);
+
+    for (int comp = 0; comp < trigRecHeader->num_requested_components; ++comp) {
+        if (READ_SIZE - num_read < sizeof(dunedaq::daqdataformats::FragmentHeader)) {
+            std::cerr << "The read buffer is too small to contain the fragment header\n";
+            return;
+        }            
+        const auto fragmentHeader = reinterpret_cast<dunedaq::daqdataformats::FragmentHeader *>(&readbuf[num_read]);
+        if (fragmentHeader->fragment_header_marker != dunedaq::daqdataformats::FragmentHeader::s_fragment_header_magic) {
+            std::cerr << "The parsed fragment header did not have the right magic number: " << std::hex << fragmentHeader->fragment_header_marker
+                      << " (expected " << dunedaq::daqdataformats::FragmentHeader::s_fragment_header_magic << ")\n";
+            return;
+        }
+
+        // make sure that all the fragments were read in
+        if (num_read + fragmentHeader->size > READ_SIZE) {
+            std::cerr << "The read buffer is too small to contain the fragment\n";
+            return;
+        }
+
+        num_read += sizeof(dunedaq::daqdataformats::FragmentHeader);
+
+        // let's see the number of frames
+        const auto frames_size = fragmentHeader->size - sizeof(dunedaq::daqdataformats::FragmentHeader);
+        const int num_frames = frames_size / sizeof(dunedaq::detdataformats::wib::WIBFrame);
+
+        for (int iframe = 0; iframe < num_frames; ++iframe) {
+            const auto frame = reinterpret_cast<dunedaq::detdataformats::wib::WIBFrame *>(&readbuf[num_read]);
+
+            // if (iframe == 0) {
+            //     outputtext << "Slot = " << frame->get_wib_header()->slot_no 
+            //         << ", fiber = " << frame->get_wib_header()->fiber_no << std::endl;
+            // }
+
+            for (int ich = 0; ich < NUM_CHANNELS; ++ich) {
+                auto chanval = frame->get_channel(ich);
+                if (chanval > channels[ich]) {
+                    channels[ich] = chanval;
+                }
+            }
+            num_read += sizeof(dunedaq::detdataformats::wib::WIBFrame);
+        }
+    }
+}
